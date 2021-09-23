5d476c3ec8ca9cc5ce244e27d768ba960c2d3c00
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2021-09-23 22:21:14 +0530

    Create firebase.js that will help connect the app with firebase backend

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBweytrE_705zEwKNtmpGg6J357k6tQ97Q",
+  authDomain: "kalapila-app.firebaseapp.com",
+  projectId: "kalapila-app",
+  storageBucket: "kalapila-app.appspot.com",
+  messagingSenderId: "522275930109",
+  appId: "1:522275930109:web:2c21d2dc57e612503a2394",
+  measurementId: "G-4TGYF6RBRN",
+};
