74fab2993ddcaf305bf48e675548b3d164f56774
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2021-09-26 12:05:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBweytrE_705zEwKNtmpGg6J357k6tQ97Q",
   authDomain: "kalapila-app.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
   appId: "1:522275930109:web:2c21d2dc57e612503a2394",
   measurementId: "G-4TGYF6RBRN",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
